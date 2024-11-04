ffd39c5628ee90f823710161ba6e4f69b966ef90
Didanny <daa50@mail.aub.edu>
2024-11-03 19:49:59 -0800

    Add all model size configs

--- train_yolo.py
+++ train_yolo.py
@@ -58,7 +58,7 @@ def parse_opt(known=False):
     parser.add_argument('--weights', type=str, default=ROOT / 'yolov5s.pt', help='initial weights path')
     parser.add_argument('--cfg', type=str, default='', help='model.yaml path')
     parser.add_argument('--data', type=str, default=ROOT / 'data/coco128.yaml', help='dataset.yaml path')
-    parser.add_argument('--hyp', type=str, default=ROOT / 'data/hyps/hyp.scratch-low.yaml', help='hyperparameters path')
+    parser.add_argument('--hyp', type=str, default='data/hyps/hyp.scratch-low.yaml', help='hyperparameters path')
     parser.add_argument('--epochs', type=int, default=100, help='total training epochs')
     parser.add_argument('--batch-size', type=int, default=16, help='total batch size for all GPUs, -1 for autobatch')
     parser.add_argument('--imgsz', '--img', '--img-size', type=int, default=640, help='train, val image size (pixels)')
@@ -137,6 +137,7 @@ def main(opt: argparse.Namespace):
     opt.save_dir = writer.log_dir
     
     # Prepare options
+    opt.hyp = ROOT / opt.hyp
     opt.data, opt.cfg, opt.hyp, opt.weights, opt.project = \
         check_file(opt.data), check_yaml(opt.cfg), check_yaml(opt.hyp), str(opt.weights), str(opt.project)  # checks
     assert len(opt.cfg) or len(opt.weights), 'either --cfg or --weights must be specified'
